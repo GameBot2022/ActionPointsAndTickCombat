1916ed0a7d6cd5886f7d85984ac27d9acef920e4
GameBot2022 <108635293+GameBot2022@users.noreply.github.com>
2025-07-16 11:45:18 +0200

    Create init.js

---
+++ scripts/init.js
@@ -0,0 +1,8 @@
+import "./cleanup.js";
+import "./quick-action-ui.js";
+// ... import other scripts as necessary
+
+Hooks.once("init", () => {
+  console.log("TickPoint Combat | Initializing module");
+  // Register your settings, formulas, and any other initialization here
+});
