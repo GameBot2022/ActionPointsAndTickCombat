11b92e2087dd53b597be4e8ced790ffec5741c09
GameBot2022 <108635293+GameBot2022@users.noreply.github.com>
2025-07-16 12:55:32 +0200

    Update gm-panel.js

--- scripts/gm-panel.js
+++ scripts/gm-panel.js
@@ -2,84 +2,81 @@
 
 const MODULE_ID = "tickpoint-combat";
 
-export class GMSettingsPanel extends FormApplication {
+// File: modules/tickpoint-combat/ui/gm-panel.js
+
+export class CustomActionManager extends FormApplication {
+  constructor(object = {}, options = {}) {
+    super(object, options);
+    this.actions = game.settings.get("tickpoint-combat", "customActions") || [];
+  }
+
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
-      id: "tickpoint-gm-panel",
-      title: "Tickpoint Combat: GM Tools",
-      template: `modules/${MODULE_ID}/templates/gm-panel.html`,
-      width: 600,
+      id: "tickpoint-custom-action-manager",
+      title: "Manage Custom Actions",
+      template: "modules/tickpoint-combat/templates/custom-actions.html",
+      classes: ["tickpoint-combat"],
+      width: 500,
       height: "auto",
-      classes: ["tickpoint-gm"],
-      closeOnSubmit: false,
-      resizable: true
+      resizable: true,
+      closeOnSubmit: false
     });
   }
 
   getData() {
     return {
-      customActions: game.settings.get(MODULE_ID, "customActions") || [],
-      exportable: true,
-      hasLogs: !!game.settings.get(MODULE_ID, "actionHistory"),
+      actions: this.actions
     };
   }
 
   activateListeners(html) {
     super.activateListeners(html);
 
-    html.find("#add-action").on("click", () => {
-      const newAction = { name: "New Action", cost: 1, category: "general", icon: "" };
-      const actions = game.settings.get(MODULE_ID, "customActions") || [];
-      actions.push(newAction);
-      game.settings.set(MODULE_ID, "customActions", actions);
-      this.render();
-    });
-
-    html.find(".delete-action").on("click", ev => {
+    html.find(".action-delete").click(ev => {
       const index = parseInt(ev.currentTarget.dataset.index);
-      const actions = game.settings.get(MODULE_ID, "customActions") || [];
-      actions.splice(index, 1);
-      game.settings.set(MODULE_ID, "customActions", actions);
-      this.render();
+      this.actions.splice(index, 1);
+      this._save();
     });
 
-    html.find(".custom-action").on("change", ev => {
+    html.find(".action-edit").click(ev => {
       const index = parseInt(ev.currentTarget.dataset.index);
-      const field = ev.currentTarget.dataset.field;
-      const value = ev.currentTarget.value;
-      const actions = game.settings.get(MODULE_ID, "customActions") || [];
-      if (actions[index]) {
-        actions[index][field] = field === "cost" ? parseInt(value) : value;
-        game.settings.set(MODULE_ID, "customActions", actions);
-      }
+      const action = this.actions[index];
+      const form = $("#edit-form");
+      form.find("[name=label]").val(action.label);
+      form.find("[name=apCost]").val(action.apCost);
+      form.find("[name=description]").val(action.description);
+      form.find("[name=icon]").val(action.icon);
+      form.find("[name=sound]").val(action.sound);
+      form.attr("data-edit-index", index);
     });
 
-    html.find("#export-log").on("click", () => this._exportLog());
-    html.find("#clear-log").on("click", () => this._clearLog());
+    html.find("#add-action-form").submit(ev => {
+      ev.preventDefault();
+      const form = ev.currentTarget;
+      const data = new FormData(form);
+      const action = Object.fromEntries(data.entries());
+      action.apCost = parseInt(action.apCost);
+      this.actions.push(action);
+      form.reset();
+      this._save();
+    });
+
+    html.find("#edit-form").submit(ev => {
+      ev.preventDefault();
+      const index = parseInt(ev.currentTarget.dataset.editIndex);
+      const form = ev.currentTarget;
+      const data = new FormData(form);
+      const action = Object.fromEntries(data.entries());
+      action.apCost = parseInt(action.apCost);
+      this.actions[index] = action;
+      form.reset();
+      delete form.dataset.editIndex;
+      this._save();
+    });
   }
 
-  async _exportLog() {
-    const logs = game.settings.get(MODULE_ID, "actionHistory") || [];
-    const data = JSON.stringify(logs, null, 2);
-    const blob = new Blob([data], { type: "application/json" });
-    const url = URL.createObjectURL(blob);
-
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "tickpoint-action-log.json";
-    a.click();
-    URL.revokeObjectURL(url);
-  }
-
-  async _clearLog() {
-    const confirmed = await Dialog.confirm({
-      title: "Clear History Log?",
-      content: "<p>This will delete all saved action logs. Are you sure?</p>"
-    });
-    if (confirmed) {
-      await game.settings.set(MODULE_ID, "actionHistory", []);
-      ui.notifications.info("Action history log cleared.");
-      this.render();
-    }
+  async _save() {
+    await game.settings.set("tickpoint-combat", "customActions", this.actions);
+    this.render();
   }
 }
