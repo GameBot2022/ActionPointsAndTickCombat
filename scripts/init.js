b6b6de16c349b6726f2436d61842b985dbf55438
GameBot2022 <108635293+GameBot2022@users.noreply.github.com>
2025-07-16 12:04:17 +0200

    Update init.js

--- scripts/init.js
+++ scripts/init.js
@@ -6,3 +6,9 @@ Hooks.once("init", () => {
   console.log("TickPoint Combat | Initializing module");
   // Register your settings, formulas, and any other initialization here
 });
+
+// Listeners to play sounds - add inside activateListeners()
+await actor.setFlag("tickpoint-combat", "currentAP", currentAP - apCost);
+await addHistoryLog(actor, `Used action: ${label}`, apCost);
+await AudioHelper.play({ src: "sounds/dice.wav", volume: 0.8, autoplay: true });
+
